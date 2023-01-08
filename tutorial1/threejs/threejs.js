ccea5300385522949b2412fd5301b4f5be4b131b
jay <dj.min43@gmail.com>
2023-01-08 17:04:31 +0900

    some nice blue card

--- tutorial1/threejs/threejs.js
+++ tutorial1/threejs/threejs.js
@@ -3,6 +3,32 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+
+const geometry = new THREE.BoxGeometry(3, 2, 0);
+const material = new THREE.MeshBasicMaterial({color: 'blue'});
+
+const cube = new THREE.Mesh(geometry, material);
+
+// default position is (0,0,0)
+scene.add(cube);
+
+// move away a bit from (0,0,0)
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 1;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
+
+
